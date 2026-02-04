662bfe35cc3687357107f2c5c1180655ee498eef
maddi-stinks <740546@student.dusd.net>
2026-02-04 11:40:52 -0800

    finishedd project
    bonus content needed lastly

--- P4PlayerControlFlores/Assets/Scripts/PlayerControl.cs
+++ P4PlayerControlFlores/Assets/Scripts/PlayerControl.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +18,11 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical Input.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontall input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
